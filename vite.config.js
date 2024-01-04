aa801970775e835d604adce53b560b509bf49e34
marco76tv <marco.sottana@gmail.com>
2024-01-04 14:34:58 +0100

    .

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+
+export default defineConfig({
+    build: {
+        outDir: __dirname + '/Resources/dist',
+        emptyOutDir: false,
+        manifest: true,
+        rollupOptions: {
+            output: {
+              entryFileNames: `assets/[name].js`,
+              chunkFileNames: `assets/[name].js`,
+              assetFileNames: `assets/[name].[ext]`
+            }
+          }
+    },
+    plugins: [
+        laravel({
+           // publicDirectory: '../../public',
+           // buildDirectory: 'build-two',
+            input: [
+                __dirname + '/Resources/css/filament/admin/theme.css',
+                __dirname + '/Resources/css/app.css',
+                __dirname + '/Resources/js/app.js',
+            ],
+            refresh: true,
+        }),
+    ],
+});
+
+//export const paths = [
+//    'Modules/$STUDLY_NAME$/resources/assets/sass/app.scss',
+//    'Modules/$STUDLY_NAME$/resources/assets/js/app.js',
+//];
