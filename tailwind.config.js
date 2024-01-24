527b0ab06c4573966320a9081e4224db8e65a29e
marco76tv <marco.sottana@gmail.com>
2024-01-24 09:05:50 +0100

    .

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,27 +1,10 @@
-import defaultTheme from 'tailwindcss/defaultTheme';
-import forms from '@tailwindcss/forms';
-import preset from '../../vendor/filament/support/tailwind.config.preset'
-
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    presets: [preset],
     content: [
-        './Resources/**/*.blade.php',
-        './Resources/**/*.js',
-        './Resources/**/*.vue',
-        './Filament/Blocks/*.php',
-        '../../resources/**/*.blade.php',
-        '../../Resources/**/*.js',
-        '../../Resources/**/*.vue',
-        '../../app/Filament/Blocks/*.php',
-        '../../vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
-        '../../storage/framework/views/*.php',
-        '../../resources/views/**/*.blade.php',
-        '../../vendor/awcodes/filament-curator/resources/**/*.blade.php',
-        '../../app/Filament/**/*.php',
-        '../../resources/views/filament/**/*.blade.php',
-        '../../vendor/filament/**/*.blade.php',
+        './resources/**/*.blade.php',
+        './resources/**/*.js',
+        './resources/**/*.vue',
+        './app/Filament/Blocks/*.php',
     ],
     theme: {
         extend: {
